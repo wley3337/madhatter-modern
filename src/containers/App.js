3ccf954e7661db03c7ccee3567605a34bcb5fd5f
wley3337 <wley3337@gmail.com>
2019-05-03 23:10:17 -0400

    fixing background for github pages

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,19 +6,11 @@ import TopHat from '../components/Tophat'
 import Fedora from '../components/Fedora'
 import ReporterHat from '../components/ReporterHat'
 import ContactIcons from './ContactIcons';
-import texturedBackground from '../images/textured-background.png'
 
 
-const backgroundImgStyle ={
-  backgroundImage: `url(${texturedBackground})`,
-  backgroundPosition: "center",
-    backgroundPepeat: "repeat",
-    backgroundSize: "cover",
-    backgroundAttachment: "fixed"
-}
 function App() {
   return (
-    <div className="main" style={backgroundImgStyle}>
+    <div className="main" >
     <div className="content-wrapper">
       <h1 className="text name-title">Will Ley</h1>
       <h3 className="welcome-text">Developer, life long learner, and wearer of many hats.</h3>
