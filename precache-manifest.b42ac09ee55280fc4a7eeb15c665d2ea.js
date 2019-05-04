ca88717106e26a94fba8262b1c115b5d5bb58479
wley3337 <wley3337@gmail.com>
2019-05-03 22:38:21 -0400

    Updates

---
+++ precache-manifest.b42ac09ee55280fc4a7eeb15c665d2ea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874c28c7b1d32e4249a63d4c369845be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00b6e68150f24ea0dd7a",
+    "url": "/static/css/main.50f4e73a.chunk.css"
+  },
+  {
+    "revision": "7b8ee2657c469ab8beba",
+    "url": "/static/js/2.f55e0352.chunk.js"
+  },
+  {
+    "revision": "00b6e68150f24ea0dd7a",
+    "url": "/static/js/main.bcc7dfc6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "68770d34fb2009f27fdd7a7168ade410",
+    "url": "/static/media/chesserOctocat.68770d34.png"
+  },
+  {
+    "revision": "6781872537e40c20c7e0ec5d8202aaa7",
+    "url": "/static/media/fedora.67818725.png"
+  },
+  {
+    "revision": "42b8de0222cf86d29327a9d34675f3af",
+    "url": "/static/media/linked-in.42b8de02.png"
+  },
+  {
+    "revision": "98e79e33c09e1534710216d515f727d6",
+    "url": "/static/media/mail.98e79e33.png"
+  },
+  {
+    "revision": "5c70230d0c498fb2d9b29beb1a7e7d9d",
+    "url": "/static/media/reporter-hat-with-card.5c70230d.png"
+  },
+  {
+    "revision": "157ebfc77409fb3380bacd389c9479cf",
+    "url": "/static/media/textured-background.157ebfc7.png"
+  },
+  {
+    "revision": "e90855c02c8f1669afa4e01cc541b272",
+    "url": "/static/media/tophat.e90855c0.png"
+  }
+]);
