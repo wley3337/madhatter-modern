b8aded78fdb17828d78fba4431693aa5dd295c2b
wley3337 <wley3337@gmail.com>
2019-05-04 11:03:11 -0400

    swapped a blog post with a new one

--- src/enumerables/blogObj.js
+++ src/enumerables/blogObj.js
@@ -1,5 +1,11 @@
 export const blogsArray = [
     {
+        id: "b4",
+        title: "Controlled dropdown selection with React, Redux, and Semantic-UI-React",
+        desc: "In this post, I walk through managing a dropdown select from Semantic UI React. It's a controlled form with the option for users to add additional items.",
+        link: "https://medium.com/@wley3337/controlled-dropdown-selection-with-react-redux-and-semantic-ui-react-6a3b2b158659"
+    },
+    {
         id: "b1",
         title: "Searchable toolbar with Semantic UI React with user input",
         desc: "A walk through on how to use Semantic UI toolbar with React without Redux.",
@@ -16,12 +22,6 @@ export const blogsArray = [
         title: "Promise.all the DIY of JavaScript",
         desc: "An extension on Promises, promises... this looks at Promise.all and it's use when you need information from multiple a-sync functions all at once in order to move forward in your programing.",
         link: "https://medium.com/@wley3337/promise-all-the-diy-of-javascript-96c068bca167"
-    },
-    {
-        id: "b4",
-        title: "Working with memory objects and their relationship to the database",
-        desc: "In this post, I walk through managing memory objects and persisted data and some of the gotcha moments when working with them, mainly updating persisted data vs memory objects.",
-        link: "https://medium.com/@wley3337/working-with-memory-objects-and-their-relationship-to-the-database-827115d43e90"
     },
     {
         id: "b5",
