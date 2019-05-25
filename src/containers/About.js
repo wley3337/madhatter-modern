f57ae865c0b39bf730e8d46b3f4d05404b632b00
wley3337 <wley3337@gmail.com>
2019-05-25 12:09:40 -0400

    misspelling error correct in about

--- src/containers/About.js
+++ src/containers/About.js
@@ -6,7 +6,7 @@ const About = () =>{
         <div className="about-wrapper text"> 
         <div className="about-text-wrapper">
 
-            <p>I came to coding through a five-day class one of my friends was offering, where we taught a robot to do basic tasks using JavaScript. It was fun, engaging, and spoke to my own problem solving process – analyzing larger problems and breaking them down into discreet manageable pieces. Building solutions to complex problems in this way has always fascinated me. With this small taste of the power of coding, I was hooked.</p>
+            <p>I came to coding through a five-day class one of my friends was offering, where we taught a robot to do basic tasks using JavaScript. It was fun, engaging, and spoke to my own problem solving process – analyzing larger problems and breaking them down into discrete manageable pieces. Building solutions to complex problems in this way has always fascinated me. With this small taste of the power of coding, I was hooked.</p>
      
             <p>Coding also connects to my passion for building community and connection. As a teacher, I’d seen the incredible impact the web had on my students and their sense of belonging. It gives groups that have felt marginalized a place where they can find others like themselves – beyond the limits of their geography - to belong to and develop a larger community. </p>
         </div>
