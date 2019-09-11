b4399a63aa9ef318992af9de91693506c763e56b
wley3337 <wley3337@gmail.com>
2019-09-11 17:18:02 -0400

    updated blog image and card to be a single image

--- src/containers/Blogs.js
+++ src/containers/Blogs.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import BlogDetail from '../components/BlogDetail'
 import { blogsArray } from '../enumerables/blogObj'
-import  blogHatSC from '../images/reporterHatSketchCardw-blog.png'
-import mhBlogs from '../images/mh-blogs.png'
+import mhBlogsWCard from '../images/mh-w-blogs.png'
 
 const Blogs =() =>{
     return(
         <div className="blogs-wrapper">
             <div className="blog-title-wrapper">
                 <span className="blog-second-img">
-                    <img src={mhBlogs} alt="Mad Hatter Running with Crumpet" />
-                </span>
-                <span className="blog-title-card">
-                    <img src={blogHatSC} className="menu-card blog-title" alt="Blog Hat Sketch Card"/>
+                <img src={mhBlogsWCard} alt="Mad Hatter Running with Crumpet and Blog Hat Card" />
                 </span>
             </div>
             <div className="blogs-details-wrapper">
