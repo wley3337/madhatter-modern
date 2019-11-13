da59ed0f41c41150f9f03f6069330c4c98453a69
wley3337 <wley3337@gmail.com>
2019-11-13 09:48:32 -0500

    removed text from titleblock"

--- src/components/TitleBlock.js
+++ src/components/TitleBlock.js
@@ -7,7 +7,6 @@ const TitleBlock = ()=>{
         <div className="title-block-wrapper">
             <span className="title-img-wrapper">
                 <img src={MadHatterWelcome} alt="Mad Hatter at tea" className="title-img"/>
-                {/* <h1 className="do-title mh-text">Do come in, won't you?</h1> */}
             </span>
             <span className="title-text-wrapper">
                 <h1 className="text name-title">Will Ley</h1>
