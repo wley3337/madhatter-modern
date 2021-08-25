3f9beeafe83ae0e52dce355886344b4028d3da22
Will Ley <wley3337@gmail.com>
2021-08-25 10:07:42 -0400

    Updated all packages and new files

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -1,48 +1,117 @@
-import React from 'react'
-import fscDetailImage from '../images/projectImages/fscDetailImages.png'
-import onitamaDetailImage from '../images/projectImages/onitamaDetailImages.png'
-import weRHereDetailImage from '../images/projectImages/weRHereDetailImages.png'
-import learnTogetherDetailImage from '../images/projectImages/learnTogetherDetailImages.png'
-import fantasyBaseball from '../images/projectImages/fantasyBaseball.png'
+import React from "react";
+
+import fantasyBaseball from "../images/projectImages/fantasyBaseball.png";
+import fscDetailImage from "../images/projectImages/fscDetailImages.png";
+import learnTogetherDetailImage from "../images/projectImages/learnTogetherDetailImages.png";
+import onitamaDetailImage from "../images/projectImages/onitamaDetailImages.png";
+import upside from "../images/projectImages/upsideDetailImages.png";
+import weRHereDetailImage from "../images/projectImages/weRHereDetailImages.png";
 
 const detailImage = (title) => {
-    switch(title){
-        case "Flatiron Students Connect":
-            return <img src={fscDetailImage} className="p-image-detail" alt="Flatiron Students connect website sample"/>
-        case "Onitama":
-            return <img src={onitamaDetailImage} className="p-image-detail" alt="Onitama website sample" /> 
-        case "WeRHere":
-            return <img src={weRHereDetailImage} className="p-image-detail" alt="We r here website sample" />
-        case "LearnTogether":
-            return <img src={learnTogetherDetailImage} className="p-image-detail" alt="Learn Together website sample" /> 
-        case 'Fantasy Baseball App':
-            return <img src={fantasyBaseball} className="p-image-detail" alt="Fantasy Baseball website login page and owner landing page" /> 
-        default:
-            return null
-    }
-}
+  switch (title) {
+    case "Upside Buisness Travel":
+      return (
+        <img
+          src={upside}
+          className="p-image-detail"
+          alt="Upside Buisness Travel website sample"
+        />
+      );
+    case "Flatiron Students Connect":
+      return (
+        <img
+          src={fscDetailImage}
+          className="p-image-detail"
+          alt="Flatiron Students connect website sample"
+        />
+      );
+    case "Onitama":
+      return (
+        <img
+          src={onitamaDetailImage}
+          className="p-image-detail"
+          alt="Onitama website sample"
+        />
+      );
+    case "WeRHere":
+      return (
+        <img
+          src={weRHereDetailImage}
+          className="p-image-detail"
+          alt="We r here website sample"
+        />
+      );
+    case "LearnTogether":
+      return (
+        <img
+          src={learnTogetherDetailImage}
+          className="p-image-detail"
+          alt="Learn Together website sample"
+        />
+      );
+    case "Fantasy Baseball App":
+      return (
+        <img
+          src={fantasyBaseball}
+          className="p-image-detail"
+          alt="Fantasy Baseball website login page and owner landing page"
+        />
+      );
+    default:
+      return <span></span>;
+  }
+};
 
-const ProjectDetail = ({title, description, githubLink, techUsed, deployedURI})=>{
-    return(
-        <div className={`project-detail-div ${title==="Onitama" ? "last" :null}` }>
-            <span className="project-info-wrapper">
-                <span className="project-detail-text">
-                    <div className="project-header">
-                        <a href={githubLink} alt="git hub project" target="_blank" rel="noopener noreferrer">
-                        <p className="project-detail-title">{title}</p>
-                        </a>
-                    </div>
-                    <p className="project-detail-desc">{description}</p>
-                    <p className="project-detail-tech"><strong>Tech used:</strong>  {techUsed}</p>
-                </span>
-                <span className="project-screen-shot">
-                    <a href={deployedURI} alt={`deployed version of ${title} `} target="_blank" rel="noopener noreferrer">
-                        { detailImage(title) }
-                    </a>
-                </span>
-            </span>
-        </div>
-    )
-}
+const ProjectDetail = ({
+  title,
+  description,
+  githubLink,
+  techUsed,
+  deployedURI,
+}) => {
+  debugger;
+  return (
+    <div
+      className={`project-detail-div ${title === "Onitama" ? "last" : null}`}
+    >
+      <span className="project-info-wrapper">
+        <span className="project-detail-text">
+          <div className="project-header">
+            {githubLink ? (
+              <a
+                href={githubLink}
+                alt="git hub project"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <p className="project-detail-title">{title}</p>
+              </a>
+            ) : (
+              <p className="project-detail-title">{title}</p>
+            )}
+          </div>
+          <p className="project-detail-desc">{description}</p>
+          <p className="project-detail-tech">
+            <strong>Tech used: </strong> {techUsed}
+          </p>
+        </span>
+        <span className="project-screen-shot">
+          {deployedURI ? (
+            <a
+              href={deployedURI}
+              alt={`deployed version of ${title} `}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {detailImage(title)}
+            </a>
+          ) : (
+            detailImage(title)
+          )}
+        </span>
+      </span>
+    </div>
+  );
+};
 
-export default ProjectDetail
+export default ProjectDetail;
--- src/enumerables/projectObj.js
+++ src/enumerables/projectObj.js
@@ -1,47 +1,62 @@
 export const projects = [
-    {
-        id: "p1",
-        title: "Fantasy Baseball App",
-        description: "A custom fantasy baseball application built for the Maelo Rivera Coqui Fantasy Baseball League. Highlights include dynamic trading with other owners, a round robin process for the daft, and a balance in automation and user driven input based on client desires. The app also includes an admin panel for dynamic client management of the current and future seasons and owners. This app is available for purchase with color and rule customization. Please email me for more information.",
-        githubLink: "https://youtu.be/oX6CMpuTtuo",
-        techUsed: "React, Socket.io, Client Side Routing, Redux, Redux-Saga, CSS Grid, JWT Token Auth, and the Semantic UI React Library, Node.js (Express), TypeScript (both front and back), PostgreSQL for persistence",
-        deployedURI: "https://youtu.be/oX6CMpuTtuo"
-    },
-    {
-        id: "p2",
-        title: "WeRHere",
-        description: "A progressive web app for people experiencing homelessness. Using Washington DC's open data APIs, it provides users with the ability to see where primary care centers, clinics, shelters, WIC and Fresh Food program locations are located throughout the city.",
-        githubLink: "https://github.com/wley3337/WeRHere",
-        techUsed: "React, Client Side Routing, Redux, Thunk, CSS Grid, and the Semantic UI React Library, and multiple external APIs (see README)",
-        deployedURI: "https://werhere.herokuapp.com/"
-    },
-    {   
-        id: "p3",
-        title: "LearnTogether",
-        description: "This is a single page web app redesign an existing Ruby on Rails only site that I created while working at Flatiron School. The new site surfaces more relevant information about how students are paired as well as makes the user experience more intuitive and aligned with modern web expectations. I designed, pitched, and engineered the project from the ground up while integrating end user feedback throughout the process. ",
-        githubLink: "https://youtu.be/-v6BXef0JiE",
-        techUsed: "React, Redux, Thunk, Client-side Routing, JWT Token Auth., CSS Grid, Semantic UI React Library, Ruby on Rails w/ PostgresSQL for persistence, and Learn Oauth for user sign-in",
-        deployedURI: "https://youtu.be/-v6BXef0JiE"
-    },   
-    {
-        id: "p4",
-        title: "Flatiron Students Connect",
-        description: "A platform where students can take and share notes and resources while going through the Flatiron software engineering course.",
-        githubLink: "https://github.com/wley3337/Flightiron-Students-Connect",
-        techUsed: "React, Client-side Routing, JWT Token Auth., Redux, Thunk, CSS Grid, and the Semantic UI React Library, Ruby on Rails w/ PostgresSQL for persistence",
-        deployedURI: "https://flatiron-students-connect.herokuapp.com/"
-    },
-    {
-        id: "p5",
-        title: "Onitama",
-        description: "A clone of the board game Onitama. It's a combination of chess and checkers where players compete using 5 of the 16 different movement cards chosen randomly at the start of the game.",
-        githubLink: "https://github.com/wley3337/onitama-frontend",
-        techUsed: "JavaScript, AJAX, HTML 5, CSS Grid, Ruby on Rails w/ PostgresSQL for persistence",
-        deployedURI: "https://onitama-front.herokuapp.com/"
-    }
-]
+  {
+    id: "p1",
+    title: "Upside Buisness Travel",
+    description:
+      "A buisness travel solution where travelers can focus on overcoming their next buisness problem, not how they're going to get to their next big meeting. \n\nI worked extensively on building out our one way rental car reservation system, led the refactoring of our booking engine and incorporation of multiple third-party travel APIs",
+    techUsed:
+      "TypeScript, NodeJS, JavaScript, Python, React, Redux, Redux-Saga, Express, SQL, GraphQL, Docker, Micro Service Architecture, Clean Architexture",
+  },
+  {
+    id: "p2",
+    title: "Fantasy Baseball App",
+    description:
+      "A custom fantasy baseball application built for the Maelo Rivera Coqui Fantasy Baseball League. Highlights include dynamic trading with other owners, a round robin process for the daft, and a balance in automation and user driven input based on client desires. The app also includes an admin panel for dynamic client management of the current and future seasons and owners. This app is available for purchase with color and rule customization. Please email me for more information.",
+    githubLink: "https://youtu.be/oX6CMpuTtuo",
+    techUsed:
+      "React, Socket.io, Client Side Routing, Redux, Redux-Saga, CSS Grid, JWT Token Auth, and the Semantic UI React Library, Node.js (Express), TypeScript (both front and back), PostgreSQL for persistence",
+    deployedURI: "https://youtu.be/oX6CMpuTtuo",
+  },
+  {
+    id: "p3",
+    title: "WeRHere",
+    description:
+      "A progressive web app for people experiencing homelessness. Using Washington DC's open data APIs, it provides users with the ability to see where primary care centers, clinics, shelters, WIC and Fresh Food program locations are located throughout the city.",
+    githubLink: "https://github.com/wley3337/WeRHere",
+    techUsed:
+      "React, Client Side Routing, Redux, Thunk, CSS Grid, and the Semantic UI React Library, and multiple external APIs (see README)",
+    deployedURI: "https://werhere.herokuapp.com/",
+  },
+  {
+    id: "p4",
+    title: "LearnTogether",
+    description:
+      "This is a single page web app redesign an existing Ruby on Rails only site that I created while working at Flatiron School. The new site surfaces more relevant information about how students are paired as well as makes the user experience more intuitive and aligned with modern web expectations. I designed, pitched, and engineered the project from the ground up while integrating end user feedback throughout the process. ",
+    githubLink: "https://youtu.be/-v6BXef0JiE",
+    techUsed:
+      "React, Redux, Thunk, Client-side Routing, JWT Token Auth., CSS Grid, Semantic UI React Library, Ruby on Rails w/ PostgresSQL for persistence, and Learn Oauth for user sign-in",
+    deployedURI: "https://youtu.be/-v6BXef0JiE",
+  },
+  {
+    id: "p4",
+    title: "Flatiron Students Connect",
+    description:
+      "A platform where students can take and share notes and resources while going through the Flatiron software engineering course.",
+    githubLink: "https://github.com/wley3337/Flightiron-Students-Connect",
+    techUsed:
+      "React, Client-side Routing, JWT Token Auth., Redux, Thunk, CSS Grid, and the Semantic UI React Library, Ruby on Rails w/ PostgresSQL for persistence",
+    deployedURI: "https://flatiron-students-connect.herokuapp.com/",
+  },
+  {
+    id: "p5",
+    title: "Onitama",
+    description:
+      "A clone of the board game Onitama. It's a combination of chess and checkers where players compete using 5 of the 16 different movement cards chosen randomly at the start of the game.",
+    githubLink: "https://github.com/wley3337/onitama-frontend",
+    techUsed:
+      "JavaScript, AJAX, HTML 5, CSS Grid, Ruby on Rails w/ PostgresSQL for persistence",
+    deployedURI: "https://onitama-front.herokuapp.com/",
+  },
+];
 
-export const inProgressProjects = [
-    
-]
-
+export const inProgressProjects = [];
